161a0d6cde1738afcf67a8a55b78fccd986683b5
0xblockbard <>
2025-10-05 13:34:49 +0700

    fix deployment issues

--- frontend/components/WalletDetails.tsx
+++ frontend/components/WalletDetails.tsx
@@ -4,6 +4,7 @@ import { LabelValueGrid } from "@/components/LabelValueGrid";
 
 export function WalletDetails() {
   const { wallet } = useWallet();
+  
   return (
     <div className="flex flex-col gap-6">
       <h4 className="text-lg font-medium">Wallet Details</h4>
@@ -11,7 +12,16 @@ export function WalletDetails() {
         items={[
           {
             label: "Icon",
-            value: wallet?.icon ? <img src={wallet.icon} alt={wallet.name} width={24} height={24} /> : "Not Present",
+            value: wallet?.icon ? (
+              <img 
+                src={wallet.icon} 
+                alt={wallet.name || "Wallet"} 
+                width={24} 
+                height={24} 
+              />
+            ) : (
+              "Not Present"
+            ),
           },
           {
             label: "Name",
@@ -20,7 +30,12 @@ export function WalletDetails() {
           {
             label: "URL",
             value: wallet?.url ? (
-              <a href={wallet.url} target="_blank" rel="noreferrer" className="text-blue-600 dark:text-blue-300">
+              <a 
+                href={wallet.url} 
+                target="_blank" 
+                rel="noreferrer" 
+                className="text-blue-600 dark:text-blue-300"
+              >
                 {wallet.url}
               </a>
             ) : (
